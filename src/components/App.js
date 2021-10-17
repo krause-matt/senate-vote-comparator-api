143cde38b45ba06f4843c25bf4a36abcd6065984
Matt Krause <krause.matt@pm.me>
2021-10-16 22:13:10 -0400

    Created button to reveal congress vote description on click event

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,21 @@ import React from "react"
 import propublica from "../api/propublica"
 
 class App extends React.Component {
+  state = { description: [] }
+
   apiSearch = async () => {
-    const response = await propublica.get("/114/senate/sessions/1/votes/3.json")
-    console.log(response)
+    const response = await propublica.get("/114/senate/sessions/1/votes/50.json")
+    console.log(response.data.results.votes.vote.description)
+    this.setState({description: response.data.results.votes.vote.description})
   }
 
   render() {
-    this.apiSearch()
     return (
-      <div>App</div>
+      <div>
+        <button onClick={this.apiSearch}>Search</button>
+        <div>{this.state.description}</div>
+      </div>
+      
     )
   }
 }
