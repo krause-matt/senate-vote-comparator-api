d49ed99cf1055024eee3b5cc164ed06b46b3d6bb
Matt Krause <krause.matt@pm.me>
2021-10-17 12:32:04 -0400

    Adding useEffect hook and inserting dropdown box for user to select which vote description they would like to get from the congress API

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,63 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import propublica from "../api/propublica"
 
-class App extends React.Component {
-  state = { description: [] }
+const App = () => {
+  const [description, setDescription] = useState("")
+  const [rollCall, setRollCall] = useState(null)
 
-  apiSearch = async () => {
-    const response = await propublica.get("/114/senate/sessions/1/votes/50.json")
-    console.log(response.data.results.votes.vote.description)
-    this.setState({description: response.data.results.votes.vote.description})
+  useEffect(() => {
+    if (rollCall) {
+      const getVote = async () => {
+        const response = await propublica.get(`/114/senate/sessions/1/votes/${rollCall}.json`)
+        setDescription(response.data.results.votes.vote.description)
+      }
+
+      getVote()
+    }
+
+  },[rollCall])
+
+  const voteNum = (e) => {
+    console.log(e)
+    setRollCall(e.target.value)
   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.apiSearch}>Search</button>
-        <div>{this.state.description}</div>
-      </div>
-      
-    )
-  }
+  return (
+    <div>
+    <select className="ui search dropdown" onChange={voteNum}>
+      <option value="">Choose Vote</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+    </select>
+      <button onClick={() => useEffect}>Search</button>
+      <div>{description}</div>
+    </div>
+  )
 }
+
+// class App extends React.Component {
+//   state = { description: [] }
+
+//   getVote = async () => {
+//     const response = await propublica.get("/114/senate/sessions/1/votes/301.json")
+//     console.log(response.data.results.votes.vote.description)
+//     this.setState({description: response.data.results.votes.vote.description})
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.getVote}>Search</button>
+//         <div>{this.state.description}</div>
+//       </div>
+      
+//     )
+//   }
+// }
 
 
 export default App
